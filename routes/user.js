bf77430e9418cf9b37187ccbb4857f83ad720352
Tian Chengyuan <395750080@qq.com>
2018-08-06 13:50:31 +0800

    dd

--- routes/user.js
+++ routes/user.js
@@ -12,7 +12,7 @@ router.get('/all',User.getAllUser); //已测
 router.get('/updateRole/:_id',User.updateUserRole);
 router.get('/delete/:_id',User.deleteUser);
 router.post('/changePassword',User.changePassword);
-// router.get('/info', User.getUserInfo);
+router.get('/info', User.getUserInfo);
 router.post('/update/:_id', User.update);
 // router.post('/changepassword', User.chanegPassword);
 
